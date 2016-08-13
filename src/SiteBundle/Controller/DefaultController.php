264dd801091215e205d286720f455302a11e6f65
Everton Pereira <suporte@agencialink.com>
2016-08-13 10:41:55 -0300

    adicionando feed

--- src/SiteBundle/Controller/DefaultController.php
+++ src/SiteBundle/Controller/DefaultController.php
@@ -20,9 +20,10 @@ class DefaultController extends Controller
      */
     public function indexAction($pagina = 1, $categoria = '')
     {
-        $limit = 20;
-        $em = $this->getDoctrine()->getManager();
-        $conteudos = $em->getRepository('SiteBundle:Conteudo')->getAllConteudo($pagina, $limit, $categoria); // Returns 5 conteudos out of 20
+        $limite = 20;
+        $dados = $this->getConteudo($pagina, $limite, $categoria);
+        $conteudos = $dados['conteudos'];
+        $categorias = $dados['categorias'];
 
         // You can also call the count methods (check PHPDoc for `paginate()`)
         # Total fetched (ie: `5` conteudos)
@@ -31,10 +32,9 @@ class DefaultController extends Controller
         # Count of ALL conteudos (ie: `20` conteudos)
         $totalConteudos = $conteudos->count();
 
-        $maxPaginas = ceil($totalConteudos / $limit);
+        $maxPaginas = ceil($totalConteudos / $limite);
         $estaPagina = $pagina;
         
-        $categorias = $em->getRepository('SiteBundle:Categoria')->findAll();
         return $this->render('site/index.html.twig',array(
                 'conteudos' => $conteudos
                 ,'categorias' => $categorias
@@ -44,21 +44,16 @@ class DefaultController extends Controller
             )
         );
     }
-    /*public function indexAction()
-    {
+    /*
+     * busca o conteudo
+     */
+    private function getConteudo($pagina = 1, $limite = 20, $categoria = ''){
         $em = $this->getDoctrine()->getManager();
-
-        $conteudos = $em->getRepository('SiteBundle:Conteudo')
-                ->findBy(
-                        array('ativo' => true)
-                        ,array('id' => 'DESC')
-                    )
-                ;
-
-        return $this->render('site/index.html.twig', array(
-            'conteudos' => $conteudos,
-        ));
-    }*/
+        $conteudos = $em->getRepository('SiteBundle:Conteudo')->getAllConteudo($pagina, $limite, $categoria); // Returns 5 conteudos out of 20
+        $categorias = $em->getRepository('SiteBundle:Categoria')->findAll();
+        
+        return array('conteudos' => $conteudos, 'categorias' => $categorias);
+    }
     
     /**
      * Finds and displays a Conteudo entity.
@@ -72,5 +67,23 @@ class DefaultController extends Controller
         return $this->render('site/show.html.twig', array(
             'conteudo' => $conteudo
         ));
+    }
+    /**
+     * @Route("/feed", name="feed_default")
+     * @Route("/feed/{categoria}", name="feed_categoria")
+     * @Route("/rss", name="feed_rss_default")
+     * @Route("/rss/{categoria}", name="feed_rss_categoria")
+     */
+    public function feedAction($categoria = '')
+    {
+        $pagina = 1;
+        $limite = 50;
+        $dados = $this->getConteudo($pagina, $limite, $categoria);
+        $conteudos = $dados['conteudos'];
+        
+        return $this->render('site/feed.xml.twig',array(
+                'conteudos' => $conteudos
+            )
+        );
     }
 }
